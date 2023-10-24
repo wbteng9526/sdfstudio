9897bf6dbe19d55fa6b3a081c4744c7a6569bff9
Wenbin Teng <wteng@smtp.ict.usc.edu>
2023-10-24 12:29:55 -0700

    render debug

--- run_eval.sh
+++ run_eval.sh
@@ -1,7 +1,6 @@
 ns-render-mesh-eval \
     --meshfile outputs/neus-facto-dtu65/neus-facto/2023-10-19_150745/tsdf_mesh.ply \
     --load_config outputs/neus-facto-dtu65/neus-facto/2023-10-19_150745/config.yml \
-    --pcfile outputs/ \
     --data_dir /home/wteng/workspace/csci677/data/DTU/scan65/ \
     --camera_path_filename meta_data.json \
     --output_dir outputs/neus-facto-dtu65/neus-facto/2023-10-19_150745/eval \
